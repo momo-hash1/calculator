6644fb69977559ea163eb0ca6560454d724c48c4
iconbit455@gmail.com <iconbit455@gmail.com>
2021-10-16 11:47:44 +0300

    basic calculator

--- public/calculator.js
+++ public/calculator.js
@@ -2,38 +2,64 @@ let calculator = {
   preValue: "",
   operator: "",
   pastValue: "",
-  result: "",
   calculate() {
-    switch (this.operator) {
+    calculator.preValue = parseInt(calculator.preValue);
+    calculator.pastValue = parseInt(calculator.pastValue);
+    let result = "";
+    switch (calculator.operator) {
       case "+":
-        this.result = this.preValue + this.pastValue;
+        result = (calculator.preValue + calculator.pastValue).toString();
+        break;
       case "-":
-        this.result = this.preValue - this.pastValue;
+        result = (calculator.preValue - calculator.pastValue).toString();
+        break;
       case "/":
-        this.result = this.preValue / this.pastValue;
+        result = (calculator.preValue / calculator.pastValue).toString();
+        break;
       case "*":
-        this.result = this.preValue * this.pastValue;
+        result = (calculator.preValue * calculator.pastValue).toString();
+        break;
     }
+    calculator.clear();
+    calculator.preValue = result;
   },
   removeValueByValue() {
-    let handleValue = calculator.operator ? "pastValue" : "preValue";
-    calculator[handleValue] = calculator[handleValue].slice(0, -1);
     if (!calculator.pastValue) {
       calculator.operator = "";
     }
+
+    let handleValue = calculator.operator ? "pastValue" : "preValue";
+    calculator[handleValue] = calculator[handleValue].slice(0, -1);
+  },
+  clear() {
+    calculator.preValue = "";
+    calculator.pastValue = "";
+    calculator.operator = "";
+    calculator.result = "";
   },
 };
 
 const updateInput = (callback) => {
   callback();
-  document.querySelector(
-    "#inputNumber"
-  ).textContent = `${calculator.preValue} ${calculator.operator} ${calculator.pastValue}`;
+  let stringView = `${calculator.preValue} ${calculator.operator} ${calculator.pastValue}`;
+  stringView = stringView.length >= 17 ? stringView.slice(-17) : stringView;
+  document.querySelector("#inputNumber").textContent = stringView;
 };
 
 const clearHandler = () => {
-  document.querySelector("#clear").addEventListener("click", () => {
-    updateInput(calculator.removeValueByValue.bind(this, calculator));
+  const clearBtn = document.querySelector("#clear");
+  clearBtn.addEventListener("click", () => {
+    updateInput(calculator.removeValueByValue);
+  });
+
+  clearBtn.addEventListener("mousedown", () => {
+    let timeout = 0;
+    clearBtn.addEventListener("mouseup", () => {
+      clearTimeout(timeout);
+    });
+    timeout = setTimeout(() => {
+      updateInput(calculator.clear)
+    }, 500);
   });
 };
 
@@ -49,16 +75,27 @@ const numberBtnHandler = () => {
   });
 };
 
+const resultBtnHandler = () => {
+  document.querySelectorAll(".result_button").forEach((el) => {
+    el.addEventListener("click", (event) => {
+      updateInput(calculator.calculate);
+    });
+  });
+};
+
 const operatorBtnHandler = () => {
   document.querySelectorAll(".operator_button").forEach((el) => {
     el.addEventListener("click", (event) => {
       updateInput(() => {
-        calculator.operator = event.target.dataset.operator;
+        if (calculator.preValue) {
+          calculator.operator = event.target.dataset.operator;
+        }
       });
     });
   });
 };
 
+resultBtnHandler();
 clearHandler();
 operatorBtnHandler();
 numberBtnHandler();
