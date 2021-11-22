c9fba313c57c0341170ec2accc1d1f3ddd0bda38
iconbit455@gmail.com <iconbit455@gmail.com>
2021-11-22 20:42:04 +0300

    switch between calculator and graph

--- public/calculator.js
+++ public/calculator.js
@@ -73,6 +73,109 @@ let calculator = {
   },
 };
 
+const initSwitcher = () => {
+  setSelectedView('calculator')
+  switcher("calculator");
+};
+
+const switcher = (type) => {
+  const htmlTemplates = {
+    calculator: `
+
+  <div class="row operation_buttons my-2">
+    <div class="col">
+      <button
+        data-operator="+"
+        class="operator_button btn btn-outline-secondary"
+        class="col"
+      >
+        +
+      </button>
+      <button
+        data-operator="-"
+        class="operator_button btn btn-outline-secondary"
+        class="col"
+      >
+        -
+      </button>
+      <button
+        data-operator="/"
+        class="operator_button btn btn-outline-secondary"
+        class="col"
+      >
+        /
+      </button>
+      <button
+        data-operator="*"
+        class="operator_button btn btn-outline-secondary"
+        class="col"
+      >
+        *
+      </button>
+      <button
+        data-operator="^"
+        class="operator_button btn btn-outline-secondary"
+        class="col"
+      >
+        ^
+      </button>
+      <button
+        data-operator="sqrt"
+        class="operator_button btn btn-outline-secondary"
+        class="col"
+      >
+        sqrt
+      </button>
+    </div>
+  </div>
+  <div class="d-flex">
+    <button data-number="1" class="number_button btn btn-outline-secondary">
+      1
+    </button>
+    <button data-number="2" class="number_button btn btn-outline-secondary">
+      2
+    </button>
+    <button data-number="3" class="number_button btn btn-outline-secondary">
+      3
+    </button>
+  </div>
+  <div class="d-flex">
+    <button data-number="4" class="number_button btn btn-outline-secondary">
+      4
+    </button>
+    <button data-number="5" class="number_button btn btn-outline-secondary">
+      5
+    </button>
+    <button data-number="6" class="number_button btn btn-outline-secondary">
+      6
+    </button>
+  </div>
+  <div class="d-flex">
+    <button data-number="7" class="number_button btn btn-outline-secondary">
+      7
+    </button>
+    <button data-number="8" class="number_button btn btn-outline-secondary">
+      8
+    </button>
+    <button data-number="9" class="number_button btn btn-outline-secondary">
+      9
+    </button>
+  </div>
+  <div class="d-flex justify-content-center align-items-center">
+    <button class="number_button btn btn-outline-secondary" data-number="0">
+      0
+    </button>
+    <button class="result_button btn btn-outline-secondary">=</button>
+  </div>
+    `,
+    graph: `
+    <canvas id="graphview-canvas"></canvas>
+    `,
+  };
+
+  document.querySelector(".switch").innerHTML = htmlTemplates[type];
+};
+
 const updateInput = (callback) => {
   callback();
 
@@ -146,20 +249,29 @@ const operatorBtnHandler = (event) => {
   });
 };
 
+const setSelectedView = (view) => {
+  const calcSideItem = document.querySelectorAll(".calc-side-item");
+
+  calcSideItem.forEach((element) => {
+    if (element.dataset.view === view) {
+      element.classList.replace("btn-light", "btn-primary");
+    } else {
+      element.classList.replace("btn-primary", "btn-light");
+    }
+  });
+};
+
 const selectView = () => {
   const calcSideItem = document.querySelectorAll(".calc-side-item");
   calcSideItem.forEach((element) => {
     element.addEventListener("click", (event) => {
-      element.classList.replace("btn-light", "btn-primary");
-      calcSideItem.forEach((element) => {
-        if (element !== event.target) {
-          element.classList.replace("btn-primary", "btn-light");
-        }
-      });
+      setSelectedView(event.target.dataset.view);
+      switcher(event.target.dataset.view)
     });
   });
 };
 
+initSwitcher();
 inputHandler(resultBtnHandler, ".result_button");
 clearHandler();
 inputHandler(operatorBtnHandler, ".operation_buttons");
