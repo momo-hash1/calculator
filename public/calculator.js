4e0e939fef190e5fa69224baa0de01e9966d7d94
momo934 <ask4r.safiullin@yandex.ru>
2022-01-06 10:39:06 +0300

    plot graph, handle null division

--- public/calculator.js
+++ public/calculator.js
@@ -15,20 +15,18 @@ let operations = {
   },
 };
 
-let parser = {
-  getHighestOperator(operationStack) {
-    let highestOperatorOrder = 0;
-    let hightestOperator = {};
-    operationStack.forEach((element, index) => {
-      if (operations[element] !== undefined) {
-        if (highestOperatorOrder < operations[element].order) {
-          hightestOperator = { operator: element, index: index };
-          highestOperatorOrder = operations[element].order;
-        }
+let getHighestOperator = (operationStack) => {
+  let highestOperatorOrder = 0;
+  let hightestOperator = {};
+  operationStack.forEach((element, index) => {
+    if (operations[element] !== undefined) {
+      if (highestOperatorOrder < operations[element].order) {
+        hightestOperator = { operator: element, index: index };
+        highestOperatorOrder = operations[element].order;
       }
-    });
-    return hightestOperator;
-  },
+    }
+  });
+  return hightestOperator;
 };
 
 let calculator = {
@@ -38,11 +36,13 @@ let calculator = {
   calculate() {
     calculator.clear();
     calculator.operationStack.shift();
+    let nullFounded = false;
     while (
-      Object.keys(parser.getHighestOperator(calculator.operationStack))
-        .length !== 0
+      Object.keys(getHighestOperator(calculator.operationStack))
+        .length !== 0 &&
+      !nullFounded
     ) {
-      let highestOperator = parser.getHighestOperator(
+      let highestOperator = getHighestOperator(
         calculator.operationStack
       );
 
@@ -52,10 +52,18 @@ let calculator = {
       let operand2 = parseFloat(
         calculator.operationStack[highestOperator.index + 1]
       );
-      calculator.operationStack.splice(highestOperator.index - 1, 2);
-      calculator.operationStack[highestOperator.index - 1] = operations[
-        highestOperator.operator
-      ].operation(operand1, operand2);
+      
+      if (operand1 === 0 || operand2 === 0) {
+        console.log('ad');
+        this.operationStack[0] = null;
+        nullFounded = true;
+      } else {
+        calculator.operationStack.splice(highestOperator.index - 1, 2);
+
+        calculator.operationStack[highestOperator.index - 1] = operations[
+          highestOperator.operator
+        ].operation(operand1, operand2);
+      }
     }
     calculator.value = calculator.operationStack[0];
     calculator.operationStack = [];
@@ -79,52 +87,78 @@ let calculator = {
     calculator.value = "";
     calculator.operator = "";
   },
+  replaceX(x) {
+    this.operationStack = this.operationStack.map((op) =>
+      op === "x" ? x : op
+    );
+  },
 };
 
 const graph = {
-  y: document.querySelector(".switch").clientWidth,
+  y: 400 / 2,
   x: document.querySelector(".switch").clientWidth / 2,
   canvas: document.querySelector("#graphview-canvas"),
-  pressed: false,
-  ctx: '',
+  currentExp: [],
+  ctx: "",
   init() {
-    this.canvas.setAttribute('width', document.querySelector('.switch').clientWidth)
-    this.canvas.setAttribute('height', 400)
-    this.render = this.render.bind(this)
-    this.render()
-    this.move()
+    this.canvas.setAttribute(
+      "width",
+      document.querySelector(".switch").clientWidth
+    );
+    this.canvas.setAttribute("height", 400);
+    this.plot_axes();
+    this.move();
   },
-  render() {
-    const ctx = graph.canvas.getContext('2d')
-    
-    ctx.clearRect(0, 0, document.querySelector('.switch').clientWidth, 400)
-    ctx.beginPath()
-    ctx.moveTo(0, graph.y)
-    ctx.lineTo(document.querySelector('.switch').clientWidth, graph.y)
-  
-    ctx.moveTo(graph.x, 0)
-    ctx.lineTo(graph.x, 400)
-  
-    ctx.stroke()
-    
-    requestAnimationFrame(this.render)
+  plot_axes() {
+    const ctx = graph.canvas.getContext("2d");
+    ctx.clearRect(0, 0, document.querySelector(".switch").clientWidth, 400);
+
+    ctx.beginPath();
+
+    ctx.moveTo(0, this.y);
+    ctx.lineTo(document.querySelector(".switch").clientWidth, this.y);
+
+    ctx.moveTo(this.x, 0);
+    ctx.lineTo(this.x, 400);
+
+    ctx.stroke();
+  },
+  drawGraph() {
+    const ctx = graph.canvas.getContext("2d");
+    let calc_x = -document.querySelector(".switch").clientWidth / 2;
+    if (this.currentExp !== 0) {
+      while (
+        Math.abs(calc_x) <=
+        document.querySelector(".switch").clientWidth / 2
+      ) {
+        calc_x += 0.1;
+        calculator.operationStack = this.currentExp.slice();
+        calculator.replaceX(calc_x);
+        calculator.calculate();
+        if (calculator.value !== null) {
+          ctx.beginPath();
+          ctx.rect(this.x - calc_x, this.y - calculator.value,0.5, 0.5);
+          ctx.stroke();
+        }
+      }
+    }
   },
   move() {
-    let prev_x = 0
-    let prev_y = 0
+    let prev_x = 0;
+    let prev_y = 0;
 
-    this.canvas.addEventListener('mousedown', (e) => {
-      prev_x = e.offsetX
-      prev_y = e.offsetY
-
-    })
-    this.canvas.addEventListener('mouseup', (e) => {
-      this.x += e.offsetX - prev_x
-      this.y += e.offsetY - prev_y
-    })
-
-  }
-}
+    this.canvas.addEventListener("mousedown", (e) => {
+      prev_x = e.offsetX;
+      prev_y = e.offsetY;
+    });
+    this.canvas.addEventListener("mouseup", (e) => {
+      this.x += e.offsetX - prev_x;
+      this.y += e.offsetY - prev_y;
+      this.plot_axes();
+      this.drawGraph();
+    });
+  },
+};
 
 const updateInput = (callback) => {
   callback();
@@ -183,8 +217,11 @@ const resultBtnHandler = () => {
   updateInput(() => {
     calculator.operationStack.push(calculator.operator, calculator.value);
     calculator.clear();
-
-    calculator.calculate();
+    if (calculator.operationStack.includes('x')) {
+      graph.currentExp = calculator.operationStack.slice();
+    }else{
+      calculator.calculate();
+    }
   });
 };
 
@@ -203,10 +240,14 @@ const switcher = {
   animation: false,
   calcSideItem: document.querySelectorAll(".calc-side-item"),
   setSelectedView(view) {
-    const prev = [...this.calcSideItem].filter((item) => item.classList.contains('btn-primary'))
+    const prev = [...this.calcSideItem].filter((item) =>
+      item.classList.contains("btn-primary")
+    );
     this.calcSideItem.forEach((element) => {
       if (element.dataset.view === view) {
-        this.changeView(this.calculateDistance(prev[0].dataset.view, element.dataset.view))
+        this.changeView(
+          this.calculateDistance(prev[0].dataset.view, element.dataset.view)
+        );
         element.classList.replace("btn-light", "btn-primary");
       } else {
         element.classList.replace("btn-primary", "btn-light");
@@ -216,37 +257,44 @@ const switcher = {
   init() {
     this.calcSideItem.forEach((element) => {
       element.addEventListener("click", (event) => {
-        this.animation = true
+        this.animation = true;
         this.setSelectedView(event.target.dataset.view);
       });
     });
-    this.calcSideItem[0].classList.replace("btn-light", "btn-primary")
+    this.calcSideItem[0].classList.replace("btn-light", "btn-primary");
   },
   calculateDistance(prev, current) {
-    let tabViews = [...this.calcSideItem]
-    tabViews = tabViews.map((el) => el.dataset.view)
-    let distance = [...tabViews].splice(tabViews.indexOf(current), 1).length
-    distance *= tabViews.indexOf(current) >= tabViews.indexOf(prev) ? -1 : 1
-    return distance * 400
+    let tabViews = [...this.calcSideItem];
+    tabViews = tabViews.map((el) => el.dataset.view);
+    let distance = [...tabViews].splice(tabViews.indexOf(current), 1).length;
+    distance *= tabViews.indexOf(current) >= tabViews.indexOf(prev) ? -1 : 1;
+    return distance * 400;
   },
   changeView(distance) {
-    const _switch = document.querySelector('.wrapper')
-    let currentScroll = 0
+    const _switch = document.querySelector(".wrapper");
+    let currentScroll = 0;
     const animateChangingView = () => {
       // TODO fix animation
 
-      if (currentScroll !== Math.abs(distance) && Math.abs(this.pos) < this.calcSideItem.length * 400) {
-        currentScroll += 20
-        if (distance < 0) { this.pos -= 20 } else { this.pos += 20 }
+      if (
+        currentScroll !== Math.abs(distance) &&
+        Math.abs(this.pos) < this.calcSideItem.length * 400
+      ) {
+        currentScroll += 20;
+        if (distance < 0) {
+          this.pos -= 20;
+        } else {
+          this.pos += 20;
+        }
       } else {
-        this.animation = false
+        this.animation = false;
       }
-      _switch.style.transform = `translateY(${this.pos}px)`
-      requestAnimationFrame(animateChangingView)
-    }
-    animateChangingView()
-  }
-}
+      _switch.style.transform = `translateY(${this.pos}px)`;
+      requestAnimationFrame(animateChangingView);
+    };
+    animateChangingView();
+  },
+};
 
 const btnHandlers = () => {
   inputHandler(resultBtnHandler, ".result_button");
@@ -256,9 +304,10 @@ const btnHandlers = () => {
 };
 
 const init = () => {
-  switcher.init()
-  graph.init()
+  calculator.value = "";
+  switcher.init();
+  graph.init();
   btnHandlers();
-}
+};
 
-init()
+init();
