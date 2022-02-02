76d0d96ba5f2800fbc64e0b8d5665534dbf25cea
momo934 <ask4r.safiullin@yandex.ru>
2022-02-02 13:48:11 +0300

    add some test for calculate

---
+++ public/tests/calculateTest.js
@@ -0,0 +1,52 @@
+import calculate from '../calculate/calculate.js'
+
+const assert = require('assert')
+
+describe('Calculate function', () => {
+    describe('should work correctly with basic expression', () => {
+        it('2+4-40+8', () => {
+            assert.equal(calculate("2+4-40+8"), -26)
+        })
+        it('2*47+7+8', () => {
+            assert.equal(calculate("2*47+7+8"), 109)
+        })
+        describe('should handle null division', () => {
+            it('2/0 + 8*2', () => {
+                assert.equal(calculate('2/0+8*2'), null)
+            })
+        })
+        describe('should work with parentheses', () => {
+            it('2 * (47 + 7) + 8', () => {
+                assert.equal(calculate('2*(47+7)+8'), 116)
+            })
+            it('2 * ((47 + 7) + 8)', () => {
+                assert.equal(calculate('2*((47+7)+8)'), 124)
+            })
+            it('(2 * 47) + (7 + 8)', () => {
+                assert.equal(calculate('(2*47)+(7+8)'), 109)
+            })
+        })
+        
+    })
+    describe('should eval sqrt functions or any other function', () => {
+        describe('Basic usage function', () => {
+            it('sqrt', () => {
+                assert.equal(calculate('sqrt(36)'), 6)
+            })
+            it('log', () => {
+                assert.equal(calculate('ln(2)'), Math.log(2))
+            })
+            it('sin', () => {
+                assert.equal(calculate('sin(36)'), Math.sin(36))
+            })
+        })
+        describe('Nested expressions', () => {
+            it('sqrt(36 + 8 * 12)', () => {
+                assert.equal(calculate('sqrt(36 + 8 * 12)'), Math.sqrt(36 + 8 * 12))
+            })
+            it('ln(2 + 89 * sin(1))', () => {
+                assert.equal(calculate('ln(2 + 89 * sin(1))'), Math.log(2 + 89 * Math.sin(1)))
+            })
+        })
+    })
+})
